5d8a37b2a36ae0b6cc0b94c49f914b8b04efcc98
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-09-29 01:57:26 +0200

    Parsing Request Paths

--- index.js
+++ index.js
@@ -5,13 +5,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
-const server = http.createServer(function(request, response) {
-    response.end('Hello World\n');
+const server = http.createServer(function (request, response) {
+    // Get the URL and parse it
+    const parsedUrl = url.parse(request.url, true);
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // Send the response
+    response.end(trimmedPath);
+    // Log the request path
+    console.log('Request received on path: ' + trimmedPath);
 });
 
 // Start the server and have it listen on port 3000
-server.listen(3000, function() {
-    console.log("Server's listening on port 3000 now")
+server.listen(3000, function () {
+    console.log("Server's listening on port 3000 now");
 });
