8b4775a12b33d1a69e65b84247e7e53aa2ccae5e
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-09-29 12:55:22 +0200

    Feature/parsing headers and payloads (#4)
    * Parsing Headers
    
    * Parsing Payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer(function (request, response) {
@@ -14,14 +15,30 @@ const server = http.createServer(function (request, response) {
     // Get the path
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    // Get the query string as an object
+    // Get the query string
     const queryString = parsedUrl.query;
     // Get the HTTP method
     const method = request.method.toLowerCase();
-    // Send the response
-    response.end(trimmedPath);
-    // Log the request path
-    console.log('Received: ' + method + ' request on '  + trimmedPath + ' with : ', queryString);
+    // Get the headers
+    const headers = request.headers;
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf8');
+    let buffer = '';
+    request.on('data', function (data) {
+        buffer += decoder.write(data);
+    });
+    request.on('end', function () {
+        buffer += decoder.end();
+        // Send the response
+        response.end(trimmedPath);
+        // Log the request path
+        console.log('Received: ' + method + ' request on '  + trimmedPath + ' with:');
+        console.log(queryString);
+        console.log('and:');
+        console.log(headers);
+        console.log('Payload:');
+        console.log(buffer);
+    });
 });
 
 // Start the server and have it listen on port 3000
