7e967e5eff895e173ba8f78b823f824d7a208a3b
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-09-29 12:42:15 +0200

    Parsing Headers

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ const server = http.createServer(function (request, response) {
     // Get the path
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    // Get the query string as an object
+    // Get the query string
     const queryString = parsedUrl.query;
     // Get the HTTP method
     const method = request.method.toLowerCase();
+    // Get the headers
+    const headers = request.headers
     // Send the response
     response.end(trimmedPath);
     // Log the request path
-    console.log('Received: ' + method + ' request on '  + trimmedPath + ' with : ', queryString);
+    console.log('Received: ' + method + ' request on '  + trimmedPath + ' with:')
+    console.log(queryString)
+    console.log('and:')
+    console.log(headers)
 });
 
 // Start the server and have it listen on port 3000
